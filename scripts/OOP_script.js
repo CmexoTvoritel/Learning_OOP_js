8c2363fa8bbc3085e7d75903280b51e847e32018
Artem <artem.sivokoz@gmail.com>
2022-11-10 18:26:16 +0300

    smth new

--- scripts/OOP_script.js
+++ scripts/OOP_script.js
@@ -1,1 +1,17 @@
-alert("Hello world");
+function isEmpty(user) {
+    for(let key in user) return false;
+    return true;
+}
+
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+alert(user.name);
+user.name = "Pete";
+alert(user.name);
+delete(user.name);
+delete(user.surname);
+alert(isEmpty(user));
+user.name = "Artem";
+alert(isEmpty(user));
