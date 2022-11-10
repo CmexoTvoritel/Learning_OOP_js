6d9a6b6f7074ebce9860f85994857a65c31203f7
Artem <artem.sivokoz@gmail.com>
2022-11-10 18:42:03 +0300

    new file with objects and new function for work with them

---
+++ scripts/OOP_objects.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+
+let salaries = {
+    John: 100,
+    Ann: 150,
+    Pete: 300,
+    Artem: 400,
+    Dima: 350,
+    Evgeniy: 275,
+}
--- scripts/OOP_script.js
+++ scripts/OOP_script.js
@@ -1,17 +1,26 @@
-function isEmpty(user) {
+"use strict";
+
+function isEmpty(user) { //check the object for empty
     for(let key in user) return false;
     return true;
 }
 
-let user = {
-    name: "John",
-    surname: "Smith",
+function sum_of_salary(salary) { //the function of counting salaries of the workers
+    let sum = 0;
+    for(let key in salary) sum+=salary[key];
+    return sum;
 }
+
 alert(user.name);
 user.name = "Pete";
 alert(user.name);
 delete(user.name);
 delete(user.surname);
+
 alert(isEmpty(user));
+
 user.name = "Artem";
+
 alert(isEmpty(user));
+
+alert(sum_of_salary(salaries));
