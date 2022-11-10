2acf0524fd79749f230cf5750a249b5e950184a7
Artem <artem.sivokoz@gmail.com>
2022-11-11 00:59:35 +0300

    new file with functions

--- scripts/OOP_script.js
+++ scripts/OOP_script.js
@@ -1,23 +1,5 @@
 "use strict";
 
-function isEmpty(user) { //check the object for empty
-    for(let key in user) return false;
-    return true;
-}
-
-function sum_of_salary(salary) { //the function of counting salaries of the workers
-    let sum = 0;
-    for(let key in salary) sum+=salary[key];
-    return sum;
-}
-
-function double_nums(menu) { //the function can double price of menu
-    for(let key in menu) {
-        if(typeof(menu[key]) == 'number')
-            menu[key] *= 2;
-    }
-}
-
 alert(user.name);
 user.name = "Pete";
 alert(user.name);
